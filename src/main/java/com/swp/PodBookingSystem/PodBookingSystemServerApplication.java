6e59c3ca5292f67a9140339675bd73a25308f4d3
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-09-18 21:45:46 +0700

    Initial commit

---
+++ src/main/java/com/swp/PodBookingSystem/PodBookingSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.swp.PodBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodBookingSystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodBookingSystemServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swp/PodBookingSystem/PodBookingSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.PodBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodBookingSystemServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
