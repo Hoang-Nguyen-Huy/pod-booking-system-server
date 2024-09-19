eac5d75aa94c8e6a52942210206bc55e9f5f3c67
Huynh Nguyen <yugivip277@gmail.com>
2024-09-19 22:58:06 +0700

    Exception handling and validation

--- src/main/java/com/swp/PodBookingSystem/mapper/AccountMapper.java
+++ src/main/java/com/swp/PodBookingSystem/mapper/AccountMapper.java
@@ -1,10 +1,13 @@
 package com.swp.PodBookingSystem.mapper;
 
 import com.swp.PodBookingSystem.dto.request.AccountCreationRequest;
+import com.swp.PodBookingSystem.dto.respone.AccountResponse;
 import com.swp.PodBookingSystem.entity.Account;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
     Account toAccount(AccountCreationRequest request);
+
+    AccountResponse toAccountResponse(Account account);
 }
