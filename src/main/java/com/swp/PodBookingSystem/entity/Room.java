f7095a4effb9617a7e88860703e6c7307c1b278e
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-09-20 13:02:17 +0700

    [PBS-40][HoangHN] feat: create entity room

---
+++ src/main/java/com/swp/PodBookingSystem/entity/Room.java
@@ -0,0 +1,37 @@
+package com.swp.PodBookingSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "room")
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String name;
+    int price;
+    String description;
+    String image;
+
+    @Value("${status:Available}")
+    String status;
+
+    @Column(name = "createdAt")
+    LocalDate createdAt;
+
+    @Column(name = "updatedAt")
+    LocalDate updatedAt;
+    int typeId;
+}
