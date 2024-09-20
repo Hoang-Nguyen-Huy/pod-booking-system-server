e3a39c5f2c64cf786095e4e436afc7c8fdd6c799
Huynh Nguyen <yugivip277@gmail.com>
2024-09-20 18:22:28 +0700

    Fix status code response

--- src/main/java/com/swp/PodBookingSystem/exception/ErrorCode.java
+++ src/main/java/com/swp/PodBookingSystem/exception/ErrorCode.java
@@ -8,9 +8,9 @@ import lombok.experimental.FieldDefaults;
 public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(500, "Uncategorized error"),
     INVALID_KEY(500, "Uncategorized error"),
-    NAME_INVALID(402, "Username must be at least 5 characters"),
-    INVALID_PASSWORD(402, "Password must be at least 8 characters"),
-    USER_NOT_EXIST(402, "User not exist"),
+    NAME_INVALID(422, "Username must be at least 5 characters"),
+    INVALID_PASSWORD(422, "Password must be at least 8 characters"),
+    USER_NOT_EXIST(422, "User not exist"),
     UNAUTHENTICATED(401, "Unauthenticated"),
     ;
 
@@ -22,11 +22,5 @@ public enum ErrorCode {
     int code;
     String message;
 
-//    public int getCode() {
-//        return code;
-//    }
-//    public String getMessage() {
-//        return message;
-//    }
 
 }
