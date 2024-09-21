43ea2b7f5178903ea886c848d42260d251ba17ec
toki-ai <lucngan2004@gmail.com>
2024-09-21 13:25:09 +0700

    adding payment

--- src/main/java/com/swp/PodBookingSystem/dto/respone/PaymentResDTO.java
+++ src/main/java/com/swp/PodBookingSystem/dto/respone/PaymentResDTO.java
@@ -1,2 +1,15 @@
-package com.swp.PodBookingSystem.dto.respone;public class PaymentResDTO {
+package com.swp.PodBookingSystem.dto.respone;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serializable;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PaymentResDTO implements Serializable {
+    String status;
+    String message;
+    String url;
 }
--- src/main/java/com/swp/PodBookingSystem/service/VNPayService.java
+++
@@ -1,2 +0,0 @@
-package com.swp.PodBookingSystem.service;public class VNPayService {
-}
