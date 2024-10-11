0734637217c71e16b4d11e3de80cb3010d346632
toki-ai <lucngan2004@gmail.com>
2024-10-11 11:08:36 +0700

    [PBS-73][Anh Dao] fix: add get order amenity repository

--- src/main/java/com/swp/PodBookingSystem/repository/OrderDetailAmenityRepository.java
+++ src/main/java/com/swp/PodBookingSystem/repository/OrderDetailAmenityRepository.java
@@ -1,2 +1,10 @@
-package com.swp.PodBookingSystem.repository;public interface OrderDetailAmenityRepository {
+package com.swp.PodBookingSystem.repository;
+
+import com.swp.PodBookingSystem.entity.OrderDetailAmenity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderDetailAmenityRepository extends JpaRepository<OrderDetailAmenity, String> {
+    List<OrderDetailAmenity> findByOrderDetailId(String orderDetailId);
 }
