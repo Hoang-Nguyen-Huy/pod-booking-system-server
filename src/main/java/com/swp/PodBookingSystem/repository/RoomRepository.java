894bde37fc7132b0f8d03ac203fe5d17f3abaf5d
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-10-18 12:23:40 +0700

    [PBS-79][HoangHN] feat: 🐒 query to get booked room

--- src/main/java/com/swp/PodBookingSystem/repository/RoomRepository.java
+++ src/main/java/com/swp/PodBookingSystem/repository/RoomRepository.java
@@ -47,5 +47,11 @@ public interface RoomRepository extends JpaRepository<Room, Integer> {
             @Param("startTime") LocalDateTime startTime,
             @Param("endTime") LocalDateTime endTime);
 
-
+    @Query("SELECT DISTINCT r FROM Room r " +
+            "JOIN OrderDetail od ON r.id = od.room.id " +
+            "WHERE od.endTime > :currentTime " +
+            "AND od.status = 'Successfully' " +
+            "AND od.customer.id = :customerId" +
+            "ORDER BY od.startTime")
+    List<Room> findRoomsWithActiveOrFutureSuccessfulBookings(@Param("currentTime") LocalDateTime currentTime, @Param("customerId") String customerId);
 }
