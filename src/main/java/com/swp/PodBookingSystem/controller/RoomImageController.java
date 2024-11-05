65500aad14b353c2483446e14200b12fb3656de6
HuyDiCode <nbquochuy.forwork@gmail.com>
2024-11-05 23:37:28 +0700

    [PBS-110][HuyNBQ] feat: create add room images by room id api in controller

--- src/main/java/com/swp/PodBookingSystem/controller/RoomImageController.java
+++ src/main/java/com/swp/PodBookingSystem/controller/RoomImageController.java
@@ -29,6 +29,17 @@ public class RoomImageController {
                 .message("Tất cả hình ảnh của phòng " + room.get().getName()+" được tìm thấy")
                 .build();
     }
+    @PostMapping("/{roomId}")
+    ApiResponse<List<String>> addRoomImages(@PathVariable int roomId, @RequestBody List<String> images) {
+        Optional<Room> room = roomRepository.findById(roomId);
+        if(room.isEmpty()) {
+            throw new RuntimeException("Không tìm thấy phòng");
+        }
+        roomImageService.addRoomImages(roomId, images);
+        return ApiResponse.<List<String>>builder().data(images)
+                .message("Thêm hình ảnh cho phòng " + room.get().getName() + " thành công")
+                .build();
+    }
 
 
 }
