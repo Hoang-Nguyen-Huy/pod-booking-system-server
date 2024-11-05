9390057c260f2c740b501b2a92bf450fff06dc88
HuyDiCode <nbquochuy.forwork@gmail.com>
2024-11-05 23:36:32 +0700

    [PBS-110][HuyNBQ] feat: create get room images by room id in controler

--- src/main/java/com/swp/PodBookingSystem/controller/RoomImageController.java
+++ src/main/java/com/swp/PodBookingSystem/controller/RoomImageController.java
@@ -1,14 +1,34 @@
 package com.swp.PodBookingSystem.controller;
 
+import com.swp.PodBookingSystem.dto.respone.ApiResponse;
+import com.swp.PodBookingSystem.entity.Room;
+import com.swp.PodBookingSystem.repository.RoomRepository;
+import com.swp.PodBookingSystem.service.RoomImageService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/room-images")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RoomImageController {
+    RoomImageService roomImageService;
+    RoomRepository roomRepository;
+    @GetMapping("/{roomId}")
+    ApiResponse<List<String>> getRoomImages(@PathVariable  int roomId) {
+        Optional<Room> room = roomRepository.findById(roomId);
+        if(room.isEmpty()) {
+            throw new RuntimeException("Không tìm thấy phòng");
+        }
+        return ApiResponse.<List<String>>builder().data(roomImageService.getRoomImages(roomId))
+                .message("Tất cả hình ảnh của phòng " + room.get().getName()+" được tìm thấy")
+                .build();
+    }
+
+
 }
