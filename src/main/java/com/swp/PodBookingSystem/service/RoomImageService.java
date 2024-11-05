09ab3e7e7db325a1d6eb4fc7d342ff91022af298
HuyDiCode <nbquochuy.forwork@gmail.com>
2024-11-05 23:38:34 +0700

    [PBS-110][HuyNBQ] feat: implement service for room image

---
+++ src/main/java/com/swp/PodBookingSystem/service/RoomImageService.java
@@ -0,0 +1,34 @@
+package com.swp.PodBookingSystem.service;
+
+import com.swp.PodBookingSystem.entity.Room;
+import com.swp.PodBookingSystem.entity.RoomImage;
+import com.swp.PodBookingSystem.repository.RoomImageRepository;
+import com.swp.PodBookingSystem.repository.RoomRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class RoomImageService {
+    RoomImageRepository roomImageRepository;
+    RoomRepository roomRepository;
+    public List<String> getRoomImages(int roomId) {
+        return roomImageRepository.findAllImagesByRoomId(roomId);
+    }
+
+    public void addRoomImages(int roomId, List<String> images) {
+
+        Optional<Room> room = roomRepository.findById(roomId);
+        images.forEach(image -> {
+            roomImageRepository.save(
+                    new RoomImage(null, image, room.orElseThrow(() -> new RuntimeException("Không tìm thấy phòng"))));
+        });
+    }
+
+}
